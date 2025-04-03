87119a2e69159098d1e98acc8a8ac493e303f22d
drumath2237 <2237k.drumath@gmail.com>
2025-04-04 01:49:20 +0900

    :sparkles: cubeを表示してカメラ制御まで

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,55 @@
 import "./style.css";
 
+import {
+  Application,
+  Entity,
+  Color,
+  FILLMODE_FILL_WINDOW,
+  RESOLUTION_AUTO
+} from 'playcanvas';
+
+// @ts-ignore
+import { CameraControls } from "playcanvas/scripts/esm/camera-controls.mjs"
+
 const main = () => {
-  const renderCanvas = document.querySelector<HTMLCanvasElement>("#renderCanvas")
-  if (!renderCanvas) {
-    return
+
+  const canvas = document.querySelector<HTMLCanvasElement>('#renderCanvas');
+  if (!canvas) {
+    throw new Error("Canvas element with id 'renderCanvas' not found");
   }
 
-  console.log("Hi");
+  const app = new Application(canvas, {
+    graphicsDeviceOptions: {
+      alpha: false
+    }
+  });
+  app.start();
+
+  app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
+  app.setCanvasResolution(RESOLUTION_AUTO);
+  window.addEventListener('resize', () => app.resizeCanvas());
+
+  const camera = new Entity('camera');
+  camera.addComponent('camera', {
+    clearColor: new Color(0.2, 0.2, 0.2)
+  });
+  camera.translate(0, 0, 5);
+  camera.addComponent("script")
+  camera.script?.create(CameraControls)
+  app.root.addChild(camera);
+
+  const light = new Entity('light');
+  light.addComponent('light', {
+    type: 'directional',
+  });
+  light.setEulerAngles(15, 30, 0);
+  app.root.addChild(light);
+
+  const box = new Entity('cube');
+  box.addComponent('model', {
+    type: 'box'
+  });
+  app.root.addChild(box);
 }
 
 main()
